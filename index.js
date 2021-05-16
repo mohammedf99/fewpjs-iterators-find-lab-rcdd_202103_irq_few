8cef7b60c0733e1d2f81586b519dedcb7705633b
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 20:53:59 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrOfObjects) {
+  
+  let result;
+  arrOfObjects.find(obj => obj.result === "W" ? result = obj.year : undefined);
+  
+  return result;
+}
